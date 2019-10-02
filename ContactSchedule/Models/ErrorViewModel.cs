b6d7aaed5bdaeb96f23e008a6bb796509a7c3bec
Leonardo Sousa <leorick97@gmail.com>
2019-10-02 20:57:29 -0300

    Create the project

---
+++ ContactSchedule/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContactSchedule.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
